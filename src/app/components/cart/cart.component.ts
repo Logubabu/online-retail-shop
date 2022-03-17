ddbc9e86c9ddd884804754fcfbc53772661e9147
Logubabu <loganathanrbl@gmail.com>
2022-03-17 16:57:37 +0530

    changes

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { storeData } from 'src/app/store';
 import { ApiService } from 'src/app/services/api.service';
+import { getData, loadData } from 'src/app/data';
 
 @Component({
   selector: 'app-cart',
@@ -12,15 +13,15 @@ export class CartComponent implements OnInit {
   constructor(private url: ApiService) { }
 
   displayedColumns: string[] = ['productName', 'orderId', 'productId', 'availableQuantity'];
-  getData: any = storeData;
+  orderdata: getData[] = storeData;
 
   ngOnInit(): void {
     console.log(storeData)
   }
   submit() {
     {
-      console.log(this.getData)
-      this.getData.map((product: any) =>{
+      console.log(this.orderdata)
+      this.orderdata.map((product: any) =>{
         this.url.placeorder(product).subscribe(res => {
           console.log(res)
         })
