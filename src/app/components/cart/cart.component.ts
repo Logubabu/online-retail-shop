8f12ae34f746eb7573fe944e90feb51a71e8e7eb
Logubabu <loganathanrbl@gmail.com>
2022-03-17 17:38:02 +0530

    finish

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { storeData } from 'src/app/store';
 import { ApiService } from 'src/app/services/api.service';
 import { getData, loadData } from 'src/app/data';
+import { v4 as uuid4 } from 'uuid';
 
 @Component({
   selector: 'app-cart',
@@ -14,18 +15,30 @@ export class CartComponent implements OnInit {
 
   displayedColumns: string[] = ['productName', 'orderId', 'productId', 'availableQuantity'];
   orderdata: getData[] = storeData;
-
+  ordercart: loadData={
+  orderId: uuid4(),
+  customerId: uuid4(),
+  productId: "",
+  quantity: 0,
+}
   ngOnInit(): void {
-    console.log(storeData)
+   
   }
   submit() {
     {
-      console.log(this.orderdata)
-      this.orderdata.map((product: any) =>{
-        this.url.placeorder(product).subscribe(res => {
+      console.log(this.ordercart)
+      this.orderdata.forEach(element => {
+        this.ordercart.productId=element.productId;
+        this.ordercart.quantity=element.availableQuantity;
+        this.url.placeorder(this.ordercart).subscribe(res=>{
           console.log(res)
         })
-      })
+      });
+      // this.ordercart.map((product: any) =>{
+      //   this.url.placeorder(product).subscribe(res => {
+      //     console.log(res)
+      //   })
+      // })
       
     }
   }
